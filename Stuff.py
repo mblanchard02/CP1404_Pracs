0f5ebc48f18b554c39eb805994355b53a7fdecbc
mblanchard02 <blanchard.m05@gmail.com>
2022-10-09 19:19:16 +1000

    Add final lists warmup file into GitHub

--- Stuff.py
+++ Stuff.py
@@ -1,2 +1,22 @@
-a = 15 / 3 + 2 * 4
-print(a)
+# names = ["Matt", "Mason", "Lachlan", "Jess", "Maddie"]
+#
+# number = int(input(f"Enter a number, up to {len(names)}: "))
+#
+# try:
+#     print(names[number - 1])
+# except IndexError:
+#     print("Invalid number")
+
+
+from operator import itemgetter
+
+score_pairs = [['Derek', 7], ['Carrie', 8], ['Bob', 6]]
+
+new_score = input("Please enter a name and a score: ") .split()
+
+new_score[1] = int(new_score[1])
+
+score_pairs.append(new_score)
+score_pairs.sort(key = itemgetter(1), reverse = True)
+
+print(score_pairs)
---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,12 @@
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
+# 3
+# 2
+# 1
+# [3, 1, 4, 1, 5, 9]
+# [1]
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print(numbers[2:6])
+print(9 in numbers)
