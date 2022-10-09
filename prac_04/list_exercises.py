642e36a473f0627a2b27e67b5f0464af74cef2e3
mblanchard02 <blanchard.m05@gmail.com>
2022-10-09 20:54:27 +1000

    Upload final list exercise file

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical
+"""
+def main():
+    numbers = []
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+
+    for i in range(5):
+        number = int(input("Number: "))
+        numbers.append(number)
+    number_details(numbers)
+
+
+def number_details(numbers):
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {(sum(numbers))/(len(numbers))}")
+
+def security_check()
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+                 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
+                 'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+                 'StartServer', 'bob']
+    username = input("Enter username:")
+    if username in usernames:
+        print("Access granted")
+        main()
+    else:
+        print("Access denied")
+
