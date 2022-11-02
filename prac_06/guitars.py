f200bf061aee93bee38922b678c2d951271a9ebd
mblanchard02 <blanchard.m05@gmail.com>
2022-11-02 15:30:54 +1000

    Submission of updated guitar practical files

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,15 +19,12 @@ def main():
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     if guitars:  # lists, strings and other collections are False when empty, True when non-empty
-        # In order for sorting to work on Guitar objects,
-        # at least the __lt__ method must be defined in Guitar class
         guitars.sort()
         print("These are my guitars:")
         for i, guitar in enumerate(guitars, 1):
             vintage_string = ""
             if guitar.is_vintage():
                 vintage_string = " (vintage)"
-            # Note the use of the format method and numbered placeholders
             print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
     else:
         print("You have no guitars. How bout you get a new one")
