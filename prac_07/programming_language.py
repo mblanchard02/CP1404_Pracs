514135f5ef50ab2bb92c6ef0997348c932b3150c
mblanchard02 <blanchard.m05@gmail.com>
2022-11-09 23:37:07 +1000

    Partial project files

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -25,20 +25,4 @@ class ProgrammingLanguage:
         return self.typing == "Dynamic"
 
 
-def run_tests():
-    """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    languages = [ruby, python, visual_basic]
-    print(python)
-
-    print("The dynamically typed languages are:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.name)
-
-
-if __name__ == "__main__":
-    run_tests()
--- prac_07/project.py
+++ prac_07/project.py
@@ -5,29 +5,24 @@ from project_management import ProjectManagement
 
 def main():
     projects = []
-    in_file = open('projects.txt', 'r')
-    in_file.readline()
-    # All other lines are language data
-    for line in in_file:
-        # print(repr(line))  # debugging
-        # Strip newline from end and split it into parts (CSV)
-        parts = line.strip().split(',')
-        # print(parts)  # debugging
-        # Reflection is stored as a string (Yes/No) and we want a Boolean
-        reflection = parts[2] == "Yes"
-        # Pointer is stored as a string (Yes/No) and we want a Boolean
-        pointer = parts[4] == "Yes"
-        # Construct a ProgrammingLanguage object using the elements
-        # year should be an int
-        project = ProjectManagement(parts[0], parts[1], reflection, pointer, int(parts[3]))
-        # Add the language we've just constructed to the list
-        projects.append(project)
-    # Close the file as soon as we've finished reading it
-    in_file.close()
-    # Loop through and display all languages (using their str method)
-    for project in projects:
-        print(project)
 
+    # opening the file in read mode
+    my_file = open("projects.txt", "r")
+
+    # reading the file
+    data = my_file.read()
+
+    # replacing end of line('/n') with ' ' and
+    # splitting the text it further when '.' is seen.
+    data_into_list = data.replace('\n', ' ').split(".")
+
+    # printing the data
+    print(data_into_list)
+    my_file.close()
+    # Close the file as soon as we've finished reading it
+    # Loop through and display all languages (using their str method)
+    # for project in projects:
+    #     print(project)
 def menu():
     print(f"Menu: \n{'(L)oad Projects'} \n{'(S)ave Projects'} \n{'(D)isplay Projects'} \n{'(F)ilter Projects by date'} \n{'(A)dd Project'} \n{'(U)pdate Project'} \n{'(Q)uit'}")
     option = input(">>>").upper()
