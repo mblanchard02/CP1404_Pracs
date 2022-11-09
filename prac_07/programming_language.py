c43aad9e1ad153652a48c63b12a23b6487064633
mblanchard02 <blanchard.m05@gmail.com>
2022-11-09 15:43:38 +1000

    Finished programming task

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,18 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, pointer, year):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
+        self.pointer = pointer
         self.year = year
+
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, Pointer Arithmetic={self.pointer}, First appeared in {self.year}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
